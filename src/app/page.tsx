fee0d3d74a7f182648702ef3687a9e5b348bcd40
fullyherge <134703250+fullyherge@users.noreply.github.com>
2025-03-27 14:39:08 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 export default async function Home() {
   const Stuff = dynamic(() => import(`./stuff.mdx`));
+  // const Stuff = await import(`./stuff.mdx`); // This doesn't work either
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Stuff />
